31754934d9e09610947a137c75ea857c139122b6
TeckKumarNitish <rskban15a2015@gmail.com>
2025-05-23 14:37:36 +0530

    added localstorage for persistance data

--- src/auth/authSlice.js
+++ src/auth/authSlice.js
@@ -2,10 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, userData: null },
+  initialState: { user: (()=>{
+    const user = localStorage.getItem("user");
+    return user ? JSON.parse(user) : null;
+  })()},
   reducers: {
-    login: (state, action) => { state.user = action.payload; },
-    logout: (state) => { state.user = null; state.userData = null; }
+    login: (state, action) => { state.user = action.payload; localStorage.setItem("user", JSON.stringify(action.payload)); },
+    logout: (state) => { state.user = null; state.userData = null; localStorage.removeItem("user"); },
   },
 });
 
